08051dadbd6f2f092169d20100d4220323200604
Ghait Jbilou <gha.jbilou@gmail.com>
2023-09-18 15:32:11 +0200

    completion trois premiere question du tp 1

--- 01-emetteurUDPv4/sender-udp.c
+++ 01-emetteurUDPv4/sender-udp.c
@@ -13,13 +13,40 @@
 
 int main (int argc, char *argv [])
 {
+    int portNbrUsr = 1;
+    
     /* test arg number */
+    
+    if (argc != 2)
+    {
+        perror("No port number indicated");
+        exit(EXIT_FAILURE);
+    }
 
     /* convert and check port number */
 
+    portNbrUsr = atoi(argv[1]);
+    if (!(portNbrUsr >= 10000 && portNbrUsr <= 65000))
+    {
+        perror("Argument given is not a port number");
+        exit(EXIT_FAILURE);
+    }
+
     /* create socket */
 
+    int sktCreated = socket(AF_INET, SOCK_DGRAM, 0);
+    CHECK(sktCreated);
+    /* le domaine indique quel protocole reseau a utiliser 
+    le type indique quel socket (nature du socket) a creer 
+    le protocole est la pour indiquer au socket quelle protocole a utilier par defaut parce que le type du socket ne connais pas le protocole a utiliser a l'origine */
+    
     /* complete sockaddr struct */
+
+    struct sockaddr_in receiver = {
+        .
+    };
+
+
 
     /* send message to remote peer */
 
