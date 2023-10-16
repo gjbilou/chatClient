c86c383d753f9d55d2b4fbfccc6edea50aaf36b0
Ghait Jbilou <gha.jbilou@gmail.com>
2023-10-16 15:10:01 +0200

    exercice 5 fini

--- 05-emetteurUDPv6/sender-udp.c
+++ 05-emetteurUDPv6/sender-udp.c
@@ -1,3 +1,4 @@
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,26 +7,74 @@
 #include <unistd.h>
 #include <netdb.h>
 
-#define CHECK(op)   do { if ( (op) == -1) { perror (#op); exit (EXIT_FAILURE); } \
-                    } while (0)
+#define CHECK(op)               \
+	do                          \
+	{                           \
+		if ((op) == -1)         \
+		{                       \
+			perror(#op);        \
+			exit(EXIT_FAILURE); \
+		}                       \
+	} while (0)
 
 #define IP "::1"
 
-int main (int argc, char *argv [])
+int main(int argc, char *argv[])
 {
-    /* test arg number */
+	int portNbrDest = 1;
 
-    /* convert and check port number */
+	/* test arg number */
 
-    /* create socket */
+	if (argc != 3)
+	{
+		fprintf(stderr, "usage: ./sender-udp ip_addr port_number\n");
+		exit(EXIT_FAILURE);
+	}
 
-    /* fill in dest IP and PORT */
+	/* convert and check port number */
 
-    /* send message to remote peer */
+	portNbrDest = atoi(argv[2]);
+	if (!(portNbrDest >= 10000 && portNbrDest <= 65000))
+	{
+		fprintf(stderr, "Argument given is not a port number\n");
+		exit(EXIT_FAILURE);
+	}
 
-    /* close socket */
+	/* create socket */
 
-    /* free memory */
+	int sktCreated = socket(AF_INET6, SOCK_DGRAM, 0);
+	CHECK(sktCreated);
 
-    return 0;
+	/* fill in dest IP and PORT */
+
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+	struct addrinfo *res;
+
+	int rtrnGAI = getaddrinfo(argv[1], argv[2], &hints, &res);
+
+	if (rtrnGAI)
+	{
+		fprintf(stderr, "%s\n", gai_strerror(rtrnGAI));
+		exit(EXIT_FAILURE);
+	}
+
+	/* send message to remote peer */
+
+	char msgSend[] = "hello world";
+	int sizeMsgSend = strlen(msgSend);
+
+	CHECK(sendto(sktCreated, msgSend, sizeMsgSend, 0, res->ai_addr, res->ai_addrlen));
+
+	/* close socket */
+
+	CHECK(close(sktCreated));
+
+	/* free memory */
+	freeaddrinfo(res);
+
+	return 0;
 }
