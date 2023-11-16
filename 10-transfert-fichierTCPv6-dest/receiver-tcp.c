2459a75fa56e5743cecc7aafc493c9a24dab7f6c
Ghait Jbilou <gha.jbilou@gmail.com>
2023-11-16 14:21:31 +0100

    fin exo 10

--- 10-transfert-fichierTCPv6-dest/receiver-tcp.c
+++ 10-transfert-fichierTCPv6-dest/receiver-tcp.c
@@ -8,48 +8,128 @@
 #include <fcntl.h>
 #include <netdb.h>
 
-#define CHECK(op)   do { if ( (op) == -1) { perror (#op); exit (EXIT_FAILURE); } \
-                    } while (0)
+#define CHECK(op)               \
+	do                          \
+	{                           \
+		if ((op) == -1)         \
+		{                       \
+			perror(#op);        \
+			exit(EXIT_FAILURE); \
+		}                       \
+	} while (0)
 
-#define IP   "::1"
-#define SIZE 100
+#define IP "::1"
+#define SIZE 1024
 #define QUEUE_LENGTH 1
 
-void cpy (int src, int dst)
+void cpy(int src, int dst)
 {
-    return;
+	ssize_t readFromFile;
+	ssize_t writtenToSkt;
+	char buff[SIZE];
+	do
+	{
+		readFromFile = read(src, buff, SIZE);
+		CHECK(readFromFile);
+
+		writtenToSkt = write(dst, buff, readFromFile);
+		CHECK(writtenToSkt);
+
+	} while (readFromFile != 0);
+	return;
 }
 
-int main (int argc, char *argv [])
+
+int main(int argc, char *argv[])
 {
-    /* test arg number */
+	int portNbrDest;
 
-    /* convert and check port number */
+	/* test arg number */
 
-    /* create socket */
+	if (argc != 3)
+	{
+		fprintf(stderr, "usage: %s ip_addr port_number\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    /* SO_REUSEADDR option allows re-starting the program without delay */
-    int iSetOption = 1;
-    CHECK (setsockopt (sockfd, SOL_SOCKET, SO_REUSEADDR, &iSetOption,
-		       sizeof iSetOption));
+	/* convert and check port number */
 
-    /* complete struct sockaddr */
+	portNbrDest = atoi(argv[2]);
+	if (!(portNbrDest >= 10000 && portNbrDest <= 65000))
+	{
+		fprintf(stderr, "Port number is incorrect\n");
+		exit(EXIT_FAILURE);
+	}
 
-    /* link socket to local IP and PORT */
+	/* create socket */
 
-    /* set queue limit for incoming connections */
+	int sktListen = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+	CHECK(sktListen);
 
-    /* wait for incoming TCP connection */
+	/* SO_REUSEADDR option allows re-starting the program without delay */
+	int iSetOption = 1;
+	CHECK(setsockopt(sktListen, SOL_SOCKET, SO_REUSEADDR, &iSetOption,
+					 sizeof iSetOption));
 
-    /* open local file */
+	/* complete struct sockaddr */
 
-    /* get the transmitted file */
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_NUMERICHOST|AI_NUMERICSERV;
+	// il faut ajouter les flags pour qu'il n'ait par chercher dans le serveur dhcp pour la completion d'addresse
+	// comme ca il ne travail que avec les addresse ip et les ports numeric
 
-    /* close sockets */
+	struct addrinfo *res;
 
-    /* close file */
+	int rtrngai = getaddrinfo(argv[1], argv[2], &hints, &res);
 
-    /* free memory */
+	if (rtrngai)
+	{
+		fprintf(stderr, "%s\n", gai_strerror(rtrngai));
+		exit(EXIT_FAILURE);
+	}
 
-    return 0;
+	/* link socket to local IP and PORT */
+
+	CHECK(bind(sktListen, res->ai_addr, res->ai_addrlen));
+
+	/* set queue limit for incoming connections */
+
+	CHECK(listen(sktListen, QUEUE_LENGTH));
+
+	/* wait for incoming TCP connection */
+
+	struct sockaddr_storage src;
+	socklen_t addrlen = sizeof(src);
+	memset(&src, 0, sizeof(addrlen));
+
+	int sktAccept = accept(sktListen, (struct sockaddr *) &src, &addrlen);
+	CHECK(sktAccept);
+
+	/* open local file */
+
+	int fdCopy = open("copy.tmp", O_CREAT|O_WRONLY|O_TRUNC, 0644);
+	CHECK(fdCopy);
+
+	/* get the transmitted file */
+
+	cpy(sktAccept, fdCopy);
+
+	/* close sockets */
+	
+	CHECK(close(sktAccept));
+	CHECK(close(sktListen));
+
+	/* close file */
+
+	CHECK(close(fdCopy));
+
+	/* free memory */
+
+	freeaddrinfo(res);
+
+	return 0;
 }
