7a2ebf8a004f85f0df36e30592fcd06f4a97412a
Ghait Jbilou <gha.jbilou@gmail.com>
2024-02-22 23:02:13 +0100

    first version of exo 12

--- 12-chat/client-chat.c
+++ 12-chat/client-chat.c
@@ -8,32 +8,89 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define CHECK(op)   do { if ( (op) == -1) { perror (#op); exit (EXIT_FAILURE); } \
-                    } while (0)
+#define CHECK(op)               \
+	do                          \
+	{                           \
+		if ((op) == -1)         \
+		{                       \
+			perror(#op);        \
+			exit(EXIT_FAILURE); \
+		}                       \
+	} while (0)
 
 #define PORT(p) htons(p)
 
-int main (int argc, char *argv [])
+int main(int argc, char *argv[])
 {
-    /* test arg number */
+	/* test arg number */
 
-    /* convert and check port number */
+	if (argc != 2)
+	{
+		fprintf(stderr, "usage: %s port_number\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    /* create socket */
+	/* convert and check port number */
 
-    /* set dual stack socket */
+	int portNbr = atoi(argv[1]);
+	if (!(portNbr >= 10000 && portNbr <= 65000))
+	{
+		fprintf(stderr, "Port number is incorrect\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	/* create socket */
 
-    /* set local addr */
+	int sktCreated = socket(AF_INET6, SOCK_DGRAM, 0);
+	CHECK(sktCreated);
 
-    /* check if a client is already present */
+	/* set dual stack socket */
 
-    /* prepare struct pollfd with stdin and socket for incoming data */
+	int value = 0;
+	CHECK (setsockopt (sktCreated, IPPROTO_IPV6, IPV6_V6ONLY, &value, sizeof value));
 
-    /* main loop */
+	/* set local addr */
 
-    /* close socket */
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+	// hints.ai_flags = AI_NUMERICHOST|AI_NUMERICSERV;
+	struct addrinfo *res;
 
-    /* free memory */
+	int rtrnGAI = getaddrinfo(NULL, argv[1], &hints, &res);
 
-    return 0;
+	if (rtrnGAI)
+	{
+		fprintf(stderr, "%s\n", gai_strerror(rtrnGAI));
+		exit(EXIT_FAILURE);
+	}
+
+	/* check if a client is already present */
+
+	int rtrnBind = bind(sktCreated, res->ai_addr, res->ai_addrlen);
+	if (rtrnBind == -1 && errno != EADDRINUSE)
+	{
+		CHECK(rtrnBind);
+	}
+	else if (rtrnBind == -1 && errno == EADDRINUSE)
+	{
+		// il faut attendre la reception du message /HELO
+	}
+	else
+	{
+		// il faut envoyer le message /HELO
+	}
+	
+
+	/* prepare struct pollfd with stdin and socket for incoming data */
+
+	/* main loop */
+
+	/* close socket */
+
+	/* free memory */
+
+	return 0;
 }
